5c29ea29bf681fcf82b349f3a32c1f9955b21a39
Florian <florian.royal@gmail.com>
2016-05-07 23:00:22 +0200

    evolution upgrade admin interace to modify value of metric for user and select instance

--- admin.php
+++ admin.php
@@ -1,3 +1,32 @@
+<?php 
+entete("Admin");
+
+if (isset($_POST)) {
+	if(isset($_POST['selectInstance']) && is_numeric($_POST['selectInstance'])) {
+		$_SESSION['instance'] = $_POST['selectInstance'];
+	}
+}
+
+if (!isset($_SESSION['instance'])) {
+	$db->query("SELECT id_instance, name_instance FROM fsf_instance");
+	$option = '';
+	if($db->get_num_rows() > 0) {
+		foreach ($db->fetch_array() as $k => $v) {
+			$option .= '<option value="'.$v['id_instance'].'">'.$v['name_instance'].'</option>';
+		}
+		
+	}
+?>
+	<h1>Choisir une instance</h1>
+	<form method="POST">
+	<label>Instance</label>
+	<select name="selectInstance"><?php echo $option ;?></select>
+	<input type="submit">
+	</form>
+	<?php 
+} else {
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,6 +44,11 @@ function change_step(select, login) {
 	ajax('aj_adm_user.php', 'login_user='+login+'&step='+select.value, 'game_user')
 }
 
+
+function change_metric_value (id_metric, login_user, value) {
+	ajax('aj_adm_user.php', 'action=change_metric&id_metric='+id_metric+'&login_user='+login_user+'&value='+value, 'game_user');
+}
+
 </script>
 </head>
 <body>
@@ -29,6 +63,9 @@ function change_step(select, login) {
 <script type="text/javascript">ajax('aj_adm_user.php', '', 'game_user');</script>
 </body>
 </html>
+<?php }
 
+footer();
+?>
 
 
--- aj_adm_user.php
+++ aj_adm_user.php
@@ -5,12 +5,17 @@ require_once ("class/db.class.php");
 $db = new Db();
 
 if(isset($_POST['login'])) {
-	$db->query("UPDATE fsf_user SET check_activate = (check_activate + 1)%2 WHERE login_user = '".$_POST['login']."' ;");
+	$db->query("UPDATE fsf_user SET check_activate = (check_activate + 1)%2 WHERE login_user = '".$_POST['login']."' and id_instance = '".$_SESSION['instance']."' ;");
 }
 
 if(isset($_POST['step'])) {
-	$db->query("UPDATE fsf_user SET id_step = '".$_POST['step']."' WHERE login_user = '".$_POST['login_user']."' ;");
+	$db->query("UPDATE fsf_user SET id_step = '".$_POST['step']."' WHERE login_user = '".$_POST['login_user']."' and id_instance = '".$_SESSION['instance']."' ;");
 }
+
+if(isset($_POST['action']) && $_POST['action'] == 'change_metric') {
+	$db->query("UPDATE fsf_user_metric SET value = value + ".$_POST['value']." WHERE login_user = '".$_POST['login_user']."' and id_metric = ".$_POST['id_metric'].";");
+}
+
 
 $select_step = '<select onchange="change_step(this);">';
 $db->query("SELECT id_step, summary FROM fsf_step");
@@ -29,29 +34,34 @@ $select_step .= '</select>';
 	<tr>
 		<th>login</th><th>activ&eacute;</th><th>step</th>
 		<?php 
-		/*
-		$db->query("SELECT id_metric, name_metric FROM fsf_metric ");
-		$metric_count = 0;
-		foreach ($db->fetch_array() as $k => $v) {
-			echo'<th>'.$v['name_metric'].'</th>';
-			$select_cause .= 'm'.$v['id_metric'] 
-			$metric_count++;
+		$db->query("SELECT name_metric from fsf_metric WHERE id_instance = '".$_SESSION['instance']."' ;");
+		if ($db->get_num_rows() > 0) {
+			foreach($db->fetch_array() as $k => $v) {
+				echo '<th>'.$v['name_metric'].'</th>';
+			}
 		}
-		*/
 		?>
-		
 	</tr>
 	</thead>
 	<tbody>
 <?php 
-$db->query("SELECT login_user, check_activate, name_instance, u.id_step, summary FROM fsf_user u join fsf_instance i on i.id_instance = u.id_instance LEFT JOIN fsf_step s ON s.id_step = u.id_step");
+$db->query("SELECT login_user, check_activate, u.id_step, summary FROM fsf_user u LEFT JOIN fsf_step s ON s.id_step = u.id_step WHERE u.id_instance = '".$_SESSION['instance']."' ;");
+$tab = $db->fetch_array();
 if ($db->get_num_rows()>0) {
-	foreach ($db->fetch_array() as $k => $v) {
+	foreach ($tab as $k => $v) {
 		if (isset($v['id_step']) && !empty($v['id_step'])) {
 			$specific_select_step = str_replace('<option value="'.$v['id_step'].'">'.$v['summary'].'</option>', '<option value="'.$v['id_step'].'" selected="selected">'.$v['summary'].'</option>', $select_step);
 			$specific_select_step = str_replace('<select onchange="change_step(this);">', '<select onchange="change_step(this,\''.$v['login_user'].'\');">', $specific_select_step);
 		}
-		echo'<tr><td>'.$v['login_user'].'</td><td><input type="checkbox" '.($v['check_activate']=='1'?'checked="checked"':'').' onchange="activate_user(\''.$v['login_user'].'\');" /></td><td>'.$specific_select_step.'</td></tr>';
+		echo'<tr><td>'.$v['login_user'].'</td><td><input type="checkbox" '.($v['check_activate']=='1'?'checked="checked"':'').' onchange="activate_user(\''.$v['login_user'].'\');" /></td><td>'.$specific_select_step.'</td>';
+		$db->query("SELECT id_metric,value FROM fsf_user_metric m where login_user = '".$v['login_user']."' ;");
+		if ($db->get_num_rows() > 0) {
+			foreach($db->fetch_array() as $k2 => $v2) {
+				echo '<td><input type="button" value="-1" onclick="change_metric_value(\''.$v2['id_metric'].'\', \''.$v['login_user'].'\', \'-1\');" /> '.$v2['value'].' <input type="button" value="+1" onclick="change_metric_value(\''.$v2['id_metric'].'\', \''.$v['login_user'].'\', \'1\');"/> </td>';
+			}
+		}
+		echo'</tr>';
+	
 	}
 }
 ?>
